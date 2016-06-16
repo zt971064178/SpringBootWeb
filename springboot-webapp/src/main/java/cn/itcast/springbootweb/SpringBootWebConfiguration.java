9b90b6aa0c1e64bd200a47e6126f40d8272de3b2
zhangtian <zhangtian@happy>
2016-06-16 22:12:57 +0800

    乱码

--- springboot-webapp/src/main/java/cn/itcast/springbootweb/SpringBootWebConfiguration.java
+++ springboot-webapp/src/main/java/cn/itcast/springbootweb/SpringBootWebConfiguration.java
@@ -1,6 +1,10 @@
 package cn.itcast.springbootweb;
 
+import javax.servlet.Filter;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -14,5 +18,15 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableWebMvc
 @EnableSwagger2
 public class SpringBootWebConfiguration extends WebMvcConfigurerAdapter {
-
+	
+	/*
+	 * 强制转换编码
+	 */
+	/*@Bean
+	public Filter characterEncodingFilter() {
+		CharacterEncodingFilter characterEncodingFilter =new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		return characterEncodingFilter;
+	}*/
 }
