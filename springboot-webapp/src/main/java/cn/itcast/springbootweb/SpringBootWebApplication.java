9d0c916c5251aa31d7952193f3ad06a8efde5599
zhangtian <zhangtian@happy>
2016-06-16 20:50:05 +0800

    发布war包准备

--- springboot-webapp/src/main/java/cn/itcast/springbootweb/SpringBootWebApplication.java
+++ springboot-webapp/src/main/java/cn/itcast/springbootweb/SpringBootWebApplication.java
@@ -3,12 +3,21 @@ package cn.itcast.springbootweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  * 启动入口
  */
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
-public class SpringBootWebApplication {
+// 发布war包继承
+public class SpringBootWebApplication extends SpringBootServletInitializer {
+	// 重写configure方法
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringBootWebApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWebApplication.class, args) ;
 	}
