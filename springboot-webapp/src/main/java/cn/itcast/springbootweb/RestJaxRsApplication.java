53aeb199a590a24d6b0d133e4755789ff5affeca
zhangtian <zhangtian@happy>
2016-06-19 14:48:48 +0800

    ddss

--- springboot-webapp/src/main/java/cn/itcast/springbootweb/RestJaxRsApplication.java
+++ springboot-webapp/src/main/java/cn/itcast/springbootweb/RestJaxRsApplication.java
@@ -6,30 +6,27 @@ import java.util.Set;
 import org.glassfish.jersey.filter.LoggingFilter;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.server.spring.scope.RequestContextFilter;
-
-import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 
 import cn.itcast.springbootweb.jeresy.server.DemoServer;
 
 public class RestJaxRsApplication extends ResourceConfig {
 	public RestJaxRsApplication() {
+	  // 向jersey框架注册资源类，凡完全限定名是以指定字符串开头的类，都将包含  
+	  packages("cn.itcast.springbootweb.jeresy.server");  
+
 	  // 注册DemoServer
 	  register(DemoServer.class);
-      register(RequestContextFilter.class);
+	  register(JacksonFeature.class);  
+      // register(RequestContextFilter.class);
       //注册数据转换器 
-      //register(JacksonJsonProvider.class); 
-      // Logging. 
-      //register(LoggingFilter.class); 
-      
-      register(CORSResponseFilter.class);
-      register(JacksonFeature.class);
-      
-    //swagger  
+      // register(JacksonJsonProvider.class); 
+      // Logging
+      register(LoggingFilter.class); 
+
+      // swagger  
      Set<Class<?>> resources = new HashSet<>();  
      resources.add(io.swagger.jaxrs.listing.ApiListingResource.class);  
      resources.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);  
      registerClasses(resources);  
-
 	}
 }
--- springboot-webapp/src/main/java/cn/itcast/springbootweb/SWaggerConfiguration.java
+++ springboot-webapp/src/main/java/cn/itcast/springbootweb/SWaggerConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.request.async.DeferredResult;
 
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
@@ -25,7 +26,7 @@ public class SWaggerConfiguration {
 					.forCodeGeneration(true)// api测试请求地址 
 					.pathMapping("/")// base，最终调用接口后会和paths拼接在一起 
 					.select() 
-					//.paths(PathSelectors.regex("/api/.*")) // 过滤的接口 
+					//.paths(PathSelectors.regex("/spring/*")) // 过滤的接口 
 					.build() 
 					.apiInfo(testApiInfo()) ; 
 	}   
