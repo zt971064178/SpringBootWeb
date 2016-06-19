7df98e20b2f20de506df1de6afb585dfceef3437
zhangtian <zhangtian@happy>
2016-06-19 16:31:03 +0800

    sss

--- springboot-webapp/src/main/java/cn/itcast/springbootweb/SWaggerConfiguration.java
+++ springboot-webapp/src/main/java/cn/itcast/springbootweb/SWaggerConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.request.async.DeferredResult;
 
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
